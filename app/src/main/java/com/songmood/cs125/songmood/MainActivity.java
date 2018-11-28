7667989c690eb36ccb604be929b6e225aeeda88b
Will Andersen <wra3@illinois.edu>
2018-11-28 11:52:59 -0600

    First COmmit

---
+++ app/src/main/java/com/songmood/cs125/songmood/MainActivity.java
@@ -0,0 +1,13 @@
+package com.songmood.cs125.songmood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
